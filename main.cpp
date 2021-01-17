abcf87c459082a3653292284f5e6ca1200496bf3
d2h10s <ecanus9@gmail.com>
2021-01-17 15:47:45 +0900

    new

--- main.cpp
+++ main.cpp
@@ -1,32 +1,3 @@
-/* Specifications
-Dual Channel 10A DC Motor Driver :: MDD10A
-┣━  정격 전압: 5V~30V
-┣━  정격 연속 전류: 10A
-┖━  정격 최대 전류: 30A (10s)
-Encoder Motor :: MB-3886E
-┣━  엔코더 분해능: 200 pulses 2Channels(광학식)
-┣━  정격 전압: DC 12/24V
-┣━  정격 토크: 100/200g.cm
-┣━  정격 회전수: 2300/4600rpm
-┣━  정격 전류: 340/680mA 이하
-┣━  무부하 회전수: 2500/5000rpm
-┖━  무부하 전류: 60/80mA
-핀배치
-┣━  Pendulum Encoder
-┃   ┣━  Orange (+)
-┃   ┣━  Brown (-)
-┃   ┣━  Black (A)
-┃   ┖━  White (B)
-┖━  Wheel
-    ┣━  Motor
-    ┃   ┣━  Red (+)
-    ┃   ┖━  Black (-)
-    ┖━  Encoder
-        ┣━  Orange (+)
-        ┣━  Brown (-)
-        ┣━  Yellow (A)
-        ┖━  Green (B)
- */
 #include "mbed.h"
 
 int main()
